3909075f58428e9dca5ce17ce9aa275a44946eea
subhash <subhash.chandra@ngtech@co.uk>
2014-12-15 15:19:13 +0530

    grid layout

---
+++ GridLayout/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GridLayout.Startup))]
+namespace GridLayout
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
